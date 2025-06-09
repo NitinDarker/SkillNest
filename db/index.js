b26624565b955bc0543e1122d5708755b097b62e
NitinDarker <nitinsharma9829547681@gmail.com>
2025-06-09 12:08:25 +0530

    improved db model structure

--- db/index.js
+++ db/index.js
@@ -6,27 +6,27 @@ const mongoURL = process.env.MONGODB_URI;
 mongoose.connect(mongoURL);
 
 const courseSchema = new Schema({
-    title: String,
-    price: Number,
-    imageUrl: String,
-    description: String,
+  title: String,
+  price: Number,
+  imageUrl: String,
+  description: String,
 });
 
 const userSchema = new Schema({
-    email: String,
-    username: String,
-    password: String,
-    purchases: courseSchema,
+  email: String,
+  username: String,
+  password: String,
+  purchases: courseSchema,
 });
 
 const adminSchema = new Schema({
-    username: String, 
-    email: String,
-    password: String,
-})
+  username: String,
+  email: String,
+  password: String,
+});
 
 const User = mongoose.model("user", userSchema);
 const Admin = mongoose.model("admin", adminSchema);
 const Course = mongoose.model("course", courseSchema);
 
-module.exports = {User, Admin, Course};
+module.exports = { User, Admin, Course };
