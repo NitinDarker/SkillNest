6a94b44a69404bc21e6bc62a3941930ad194f44f
NitinDarker <nitinsharma9829547681@gmail.com>
2025-06-10 16:03:24 +0530

    user auth

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -1,15 +1,17 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const jwtKey = process.env.JWT_KEY;
 
 function userAuth(req, res, next) {
   const token = req.headers.authorization;
+
   try {
     jwt.verify(token, jwtKey);
   } catch (e) {
     console.log(e);
-    res.status(300).send("User does not exist");
+    return res.status(401).send("User does not exist");
   }
   next();
 }
- 
+
 module.exports = { userAuth };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,19 @@
+const jwt = require("jsonwebtoken");
+const { z } = require("zod");
+const { userAuth } = require("../middlewares/userAuth");
 const { Router } = require("express");
 const { User, Course } = require("../db/index");
-const jwt = require("jsonwebtoken");
-const { userAuth } = require("../middlewares/userAuth")
+const { email } = require("zod/v4");
 
+require("dotenv").config();
 const userRouter = Router();
 const jwtKey = process.env.JWT_KEY;
+
+const ZodUser = z.object({
+  username: z.string().min(1).max(12),
+  email: z.string(email),
+  password: z.string().min(4).max(14),
+});
 
 userRouter.post("/signup", async (req, res) => {
   const email = req.body.email;
@@ -17,15 +26,21 @@ userRouter.post("/signup", async (req, res) => {
     password: password,
   });
 
+  if (!ZodUser.safeParse(newUser).success) {
+    // => { success: true; data: "tuna" }
+    return res.status(400).send("Invalid Credentials!");
+  }
+
   await newUser.save();
+
   const token = jwt.sign({ username: newUser.username }, jwtKey);
   res.status(200).json({
-    token: token
+    token: token,
   });
 });
 
 userRouter.post("/login", userAuth, (req, res) => {
-    res.send("Verified")
+  res.send("Verified");
 });
 
 userRouter.get("/courses", (req, res) => {});
